ed2a5235ebe46464308035d6b68d8bd587f5f90d
Aayush Gupta <aayush.gupta555@gmail.com>
2023-02-10 14:57:46 +0530

    Added Collectibles
    Created a collectible object and implemented animation

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    private Animator animator;
+    private void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>() != null)
         {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             playerController.Pickup_Key();
-            Destroy(gameObject);
+            animator.SetBool("Collected", true);
+            StartCoroutine(End());
         }
+    }
+
+    IEnumerator End()
+    {
+        yield return new WaitForSeconds(0.4f);
+        Destroy(gameObject);
     }
 }
